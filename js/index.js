26384de1695f932be9987dd78fe5842943ca34e5
gpablosl <69810373+gpablosl@users.noreply.github.com>
2023-08-22 18:29:11 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,9 +3,19 @@ function calcularSuma(){
     n1 = $("#txtN1").val();
     n2 = $("#txtN2").val();
 
-    res = parseInt(n1) + parseInt(n2)
+    if(n1=="" || n2 == "")
+    {
+        swal.fire(
+            "Error",
+            "Debe capturar números para N1 y N2",
+            "error"
+        );
+    }
+    else{
+        res = parseInt(n1) + parseInt(n2)
 
-    $("#txtRes").val(res)
+        $("#txtRes").val(res)
+    }
 
  }
 
@@ -13,8 +23,18 @@ function calcularSuma(){
     n1 = $("#txtN1").val();
     n2 = $("#txtN2").val();
 
-    res = parseInt(n1) - parseInt(n2)
+    if(n1=="" || n2 == "")
+    {
+        swal.fire(
+            "Error",
+            "Debe capturar números para N1 y N2",
+            "error"
+        );
+    }
+    else{
+        res = parseInt(n1) - parseInt(n2)
 
-    $("#txtRes").val(res)
+        $("#txtRes").val(res)
+    }
 
  }
