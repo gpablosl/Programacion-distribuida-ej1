9945a0ebab2b574466de42a04474a5a618549da3
gpablosl <69810373+gpablosl@users.noreply.github.com>
2023-08-22 18:21:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href ="vendor\sweetalert2\dist\sweetalert2.min.css">
 </head>
 <body>
-    <div>
         <input type="text" name="txtN1" id="txtN1" value="0">
         <span>+/-</span>
         <input type="text" name="txtN2" id="txtN2" value="0">
@@ -16,9 +15,6 @@
         <input type="text" name="txtRes" id="txtRes" value="0">
         <button onclick="calcularSuma();">Calcular Suma</button>
         <button onclick="calcularResta();">Calcular Resta</button>
-
-    </div>
-
     <script src="vendor/jquery/jquery-3.7.0.js"></script>
     <script src="js/index.js"></script>
     <script src="vendor\sweetalert2\dist\sweetalert2.all.min.js"></script>
