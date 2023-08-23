503af0d2b460da14e9faddac6208294c5a4df36f
gpablosl <69810373+gpablosl@users.noreply.github.com>
2023-08-22 18:20:12 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function calcularSuma(){
+
+    n1 = $("#txtN1").val();
+    n2 = $("#txtN2").val();
+
+    res = parseInt(n1) + parseInt(n2)
+
+    $("#txtRes").val(res)
+
+ }
+
+ function calcularResta(){
+    n1 = $("#txtN1").val();
+    n2 = $("#txtN2").val();
+
+    res = parseInt(n1) - parseInt(n2)
+
+    $("#txtRes").val(res)
+
+ }
