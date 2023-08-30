97fee4c7c45aed6120da21a723d91e436043cade
gpablosl <69810373+gpablosl@users.noreply.github.com>
2023-08-29 17:56:51 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cactus</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href ="vendor\sweetalert2\dist\sweetalert2.min.css">
 </head>
 <body>
-        <input type="text" name="txtN1" id="txtN1" value="0">
-        <span>+/-</span>
-        <input type="text" name="txtN2" id="txtN2" value="0">
-        <span>=</span>
-        <input type="text" name="txtRes" id="txtRes" value="0">
-        <button onclick="calcularSuma();">Calcular Suma</button>
-        <button onclick="calcularResta();">Calcular Resta</button>
+    <div class="container" id="dvContainer">
+        
+    </div>
+
     <script src="vendor/jquery/jquery-3.7.0.js"></script>
     <script src="js/index.js"></script>
     <script src="vendor\sweetalert2\dist\sweetalert2.all.min.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/1c4528e88d.js" crossorigin="anonymous"></script>
 </body>
 </html>
