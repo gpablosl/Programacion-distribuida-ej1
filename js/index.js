1a71fbb1cb067f2e2571bac3c9105bcbac5d2f3c
gpablosl <69810373+gpablosl@users.noreply.github.com>
2023-08-29 18:06:08 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -38,3 +38,16 @@ function calcularSuma(){
     }
 
  }
+
+ function getClientes(){
+    $.post("modulos/clientes/getClientes.php",{})
+    .done(function(data)
+    {
+        $("advContainer").html("");
+        $("advContainer").html(data);
+    });
+ }
+
+ $( document ).ready( function(){
+    getClientes();
+})
